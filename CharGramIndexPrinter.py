4fc19aa302efaf8444a8318bbdf251256f9c1d60
Aarsh Patel <patelaarsh10@gmail.com>
2022-02-10 15:26:21 -0800

    Update CharGramIndexPrinter.py
    Fixing and finalizing output entries

--- CharGramIndexPrinter.py
+++ CharGramIndexPrinter.py
@@ -1,15 +1,13 @@
 import argparse
 
-# Corpus Documents
-documents = {}
-
 
 # Clean up and Process string lines
-def process_str(c):
+def process_str(c, n):
     result = " ".join(c.split())
     result = result.lower()
     result = result.replace(" ", "_")
-    return "___" + result + "___"
+    padding = ''.join(["_" for _ in range(n - 1)])
+    return padding + result + padding
 
 
 # Generate N-grams for each document
@@ -18,6 +16,7 @@ def gen_ngram(c, n):
     lst = []
     length = len(c)
 
+    # Divide and insert same amount of words
     while ptr < length:
         if ptr == (length - (n - 1)):
             break
@@ -27,32 +26,73 @@ def gen_ngram(c, n):
     return lst
 
 
+# Generate N-grams database
+def gen_database(d, n):
+    # Corpus Database
+    database = {}
+
+    for idx, data in enumerate(d):
+        line = process_str(data, n)
+        ngram = gen_ngram(line, n)
+
+        for pos, d in enumerate(ngram):
+            if d in database:
+                # Update exiting entry
+                database[d][2].add((idx, pos))
+            else:
+                # First entry
+                database[d] = [1, 1, {(idx, pos)}]
+
+        # Update entries
+        for e in database.values():
+
+            # Count numbers of entries in each
+            doc_count = set()
+            for c in e[2]:
+                doc_count.add(c[0])
+
+            # Update entries counts
+            e[0] = len(doc_count)
+            e[1] = len(e[2])
+
+    return database
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="get corpus data", type=str)
-    parser.add_argument("ngram", help="get ngram value", type=int)
+    parser.add_argument("ngram", help="get ngram count", type=int)
     args = parser.parse_args()
 
-    # Read file to get corpus document
+    # Read file to get corpus data
     with open(args.filename) as file:
         docs = []
-        line = []
+        lines = []
 
+        # Right strip lines to remove new lines
         fLines = [line.rstrip() for line in file]
-        for i, s in enumerate(fLines):
-            if '---' not in s:
-                line.append(s)
+
+        # Get each document data
+        for i, l in enumerate(fLines):
+            # Document seperator
+            if '---' not in l:
+                lines.append(l)
             else:
-                docs.append(''.join(line))
-                line = []
+                docs.append(''.join(lines))
+                lines = []
 
+            # End line
             if i == len(fLines) - 1:
-                docs.append(''.join(line))
+                docs.append(''.join(lines))
 
-        # Generate NGram for Each document
-        for i, data in enumerate(docs):
-            line = process_str(data)
-            ngram = gen_ngram(line, args.ngram)
-            documents[i] = ngram
+        # Generate N-Grams database
+        corpus_data = gen_database(docs, args.ngram)
+        # print(corpus_data)
 
-        print(documents)
+        # Soring database
+        sorted_data = sorted(corpus_data.keys(), key=lambda x: x.lower())
+
+        # Printing reverse lookup output
+        for corp in sorted_data:
+            entry = corpus_data[corp]
+            print("{}\n{},{},{}".format(corp, entry[0], entry[1], ','.join([str(x) for x in entry[2]])))
